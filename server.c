64ef725a377c0a51c79973b82d865ab2f8cba757
Wade Cline <wadecline@hotmail.com>
2015-04-12 08:28:57 -0700

    Initial commit.

---
+++ server.c
@@ -0,0 +1,27 @@
+/**
+ *  Implementation of Dunbar's "Glass Plate Game" game, which is based off of
+ *  Herman Hesse's novel, "The Glass Bead Game".
+ *
+ *  This is the server's portion of the code.
+ *
+ *  Copyright (C) 2015  "Frostsnow" (Wade T. Cline).
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("The lattice of a million minds begins with a single connection.\n");
+}
